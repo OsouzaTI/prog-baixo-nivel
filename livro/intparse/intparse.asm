c6dd6b42aefab1d09570cbb0c9553959136df2f0
osouzaTI <o.souzati@gmail.com>
2022-01-15 18:20:32 -0400

    intparse / intprint

--- livro/intparse/intparse.asm
+++ livro/intparse/intparse.asm
@@ -6,12 +6,11 @@
 %endmacro
 
 global _start
-global newline
-global strprint
 
 section .data
-newln0: db 10
+numero: db "-123", 0
 symb_negative: db 45
+newln0: db 10
 
 section .text
 
@@ -81,8 +80,42 @@ intprint:
     add rsp, rcx    ; liberando topo da pilha de hardware
     ret
 
-_start:
-    mov rax, -2021  ; numero a ser impresso
-    call intprint   ; chamando funcao
-    exit 0          ; saida 0 -> sem erros
+intparse:
+    xor rcx, rcx    ; limpa rax
+    xor rdx, rdx    ; limpa rdx
+    xor rsi, rsi    ; flag de numero negativo   
+.loop:
+    
+    movzx r8, byte [rdi + rcx]
+    test r8, r8
+    je .end
 
+    cmp r8, 0x2d    ; verifica se tem o symbolo '-'
+    je .L1          ; jump se for igual
+    
+    sub r8, 0x30    ; transforma de ascii para decimal
+    imul rdx, 10
+    add rdx, r8
+    jmp .L2
+.L1:                ; numero negativo
+    mov rsi, 1      ; definido como (0) positivo | (1) negativo
+.L2:
+    inc rcx    
+    jmp .loop
+
+.L3:
+    neg rdx         ; inverte o sinal do numero
+    ret             ; retorna
+.end:
+    cmp rsi, 1      ; se for negativo
+    je  .L3         ; jump se for igual
+    ret             ; retorna
+
+_start:
+    mov rdi, numero
+    call intparse
+    mov rax, rdx
+    add rax, 10
+    call intprint
+
+    exit 0
